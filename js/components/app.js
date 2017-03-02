6cd3b27a16c2d2c574ac950e17e5ffd5ae13d660
debelopumento <imdiye@gmail.com>
2017-03-02 11:39:28 -0800

    made inbox and spambox variable routes

--- js/components/app.js
+++ js/components/app.js
@@ -4,8 +4,21 @@ import Inbox from './inbox'
 import { Link } from 'react-router'
 
 const App = (props) => {
+	const mailboxes = ['inbox', 'spam'].map((mailbox, index) => {
+		return (
+			<li key={index}>
+				<NavLink to={`/${mailbox}`}>{mailbox}</NavLink>
+			</li>
+		)
+	})
+
+
+
 	return 	<div>
 				<h1>Your emails</h1>
+				<ul role='nav'>
+					{mailboxes}
+				</ul>
 				<ul role='nav'>
 					<li><NavLink to='/inbox'>Inbox</NavLink></li>
 					<li><NavLink to='/spambox'>Spambox</NavLink></li>
